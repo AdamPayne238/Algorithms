dc6ad2cbb1206d702a17050707bcac2d65cfd27b
Adam Payne <ajpayne238@gmail.com>
2020-02-20 15:33:19 -0500

    finished recipe_batches and stock_prices. eating_cookies was covered in lecture. Moving on to rock_paper_scissors

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,34 @@
 
 import math
 
+
+# Write a function that receives a recipe and ingredients available to you in a dictionary
 def recipe_batches(recipe, ingredients):
-  pass 
+
+    batches = 0
+
+    while True:
+
+        for value in recipe:
+            # if we dont have all the ingredients needed for recipe, exit
+            if ingredients.get(value) is None:
+                return batches  # returns 0
+
+            else:
+                # values in ingredients = values of ingredients - values of recipes
+                ingredients[value] = ingredients.get(value) - recipe.get(value)
+                # if value of ingredients is less than 0, return batches
+                if ingredients[value] < 0:
+                    return batches
+
+        # iterate batches by 1
+        batches += 1
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,44 @@
 # Start
 import argparse
 
-def find_max_profit(prices):
-  pass
+# Test Price Lis
+# prices1 = [10, 7, 5, 8, 11, 9]
 
 
+# prices2 = [100, 90, 80, 50, 20, 10]
+prices = [1050, 270, 1540, 3800, 2]
+
+
+# For example, find_max_profit([1050, 270, 1540, 3800, 2]) should return 3530
+def find_max_profit(prices):  # Receives an input list of stock prices
+
+    # min and max return the smallest and largest numbers in the list
+    max_profit = min(prices) - max(prices)
+
+    # Buy Price
+    for buy in range(len(prices)):
+
+        # Sell Price
+        for sell in range(buy + 1, len(prices)):
+            min_price = prices[buy]     # range(len(prices))
+            max_price = prices[sell]    # range(buy + 1, len(prices))
+            profit = max_price - min_price
+            if profit > max_profit:
+                max_profit = profit
+
+    print(max_profit)
+    return max_profit
+
+
+# Test Cal
+find_max_profit(prices)
+# find_max_profit((prices2))
+# find_max_profit((prices3))
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
